a432cb2ae0f39a5ac23cbcd7a9a19e323800ebd9
chmousNedovolniy <89921408+chmousNedovolniy@users.noreply.github.com>
2022-04-03 19:08:49 +0300

    Update small_how_read_input.cpp

--- my_helpers/small_how_read_input.cpp
+++ my_helpers/small_how_read_input.cpp
@@ -6,7 +6,7 @@ int main() {
     // Наиболее часто граф задают последовательностью ребёр на node_count вершинах.
     // В таком случае граф стоит считать так:
     // Добавляем верщины в граф
-    std::vector<Graph<int>::Node*> nodes;
+    std::vector<Graph<int>::Node*> nodes(node_count);
     for (int i = 0; i < node_count; ++i) {
         nodes[i] = graph.addNode(i);
     }
@@ -14,10 +14,10 @@ int main() {
     // Теперь считываем рёбра
     for (int i = 0; i < edge_count; ++i) {
         int from, to; // считаем, что вершины пронумерованы с нуля
-        // если нумерация с единицы, стоит сделать --from; --to;
         int weight; // иногда дают, иногда нет
         // long long weigth; // если большой вес
         std::cin >> from >> to >> weight;
+         // если нумерация с единицы, стоит сделать --from; --to;
         graph.addEdge(nodes[from], nodes[to], weight); // в случае неориентированного графа
         graph.addDirectedEdge(nodes[from], nodes[to], weight); // в случае ориентированного графа
     }    
